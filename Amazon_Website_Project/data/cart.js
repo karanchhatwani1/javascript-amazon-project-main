0c123b6f13a594555eabc0900e99049b858f3788
karanchhatwani1 <karanchh2001@gmail.com>
2024-10-02 18:29:13 +0530

    Save cart in local storage

--- Amazon_Website_Project/data/cart.js
+++ Amazon_Website_Project/data/cart.js
@@ -1,10 +1,18 @@
-export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1 
-}];
+export let cart = JSON.parse(localStorage('cart'));
+
+if(!cart){
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1 
+    }];
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let addedMessageTimeoutId;
@@ -44,6 +52,7 @@ export function addToCart(productId){
     }, 2000);
   
     addedMessageTimeoutId = timeOutId;
+    saveToStorage();
   }
 
   export function removeFromCart(productId){
@@ -56,4 +65,5 @@ export function addToCart(productId){
     });
 
     cart = newCart;
+    saveToStorage();
   }
